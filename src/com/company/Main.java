bfbaf4db94894a9eb2aa9d32b36ba59a000fa343
vipmax <djvipmax@gmail.com>
2014-03-06 15:37:06 +0400

    отлично печатает из картинки уже картинка редактируется можно начинать делать UI

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.print.PageFormat;
-import java.awt.print.Printable;
-import java.awt.print.PrinterException;
-import java.awt.print.PrinterJob;
+import java.awt.print.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -15,38 +12,42 @@ public class Main implements Printable{
 
 
     public static void main(String[] args) throws PrinterException, IOException {
-	paint();
+	    paint();
 		img = new ImageIcon("image.jpg").getImage();
-		PrinterJob printerJob = PrinterJob.getPrinterJob();
-		PageFormat pf = printerJob.defaultPage();
-		System.out.println(pf.getPaper().getHeight() + " x " + pf.getPaper().getHeight());
-		pf.setOrientation(PageFormat.LANDSCAPE);
-		printerJob.setPrintable(new Main(), pf);
+		PrinterJob pPrinterJob = PrinterJob.getPrinterJob();
+		PageFormat pPageFormat = pPrinterJob.defaultPage();
+		Paper pPaper = pPageFormat.getPaper();
+		pPageFormat.setOrientation(PageFormat.LANDSCAPE);
+		pPaper.setSize(594.992125984252, 841.8897637795276);
+		pPaper.setImageableArea(0.1, 0, 522.99212598425197, 769.8897637795276);
+		pPaper.setImageableArea(0, 0, pPaper.getWidth() - 2.0, pPaper.getHeight() - 2.0);
+
+		pPageFormat.setPaper(pPaper);
+		pPageFormat = pPrinterJob.pageDialog(pPageFormat);  //без диалога печатает с большим отступом
+		Book pBook = new Book();
+		pBook.append(new Main(), pPageFormat);
+		pPrinterJob.setPageable(pBook);
 
 
-		if(printerJob.printDialog()==true){                // если нажат ок на диалоге выбора принтера
+		if (pPrinterJob.printDialog()) {
 
-			printerJob.print();
+			pPrinterJob.print();
 		}
     }
 	public static void paint() throws IOException {
-		int width = 1169*2 ,height = 827*2 ;
-		BufferedImage img = new BufferedImage(width , height , BufferedImage.TYPE_INT_ARGB_PRE);
+
+		BufferedImage img = ImageIO.read(new File("C:\\forma_6-page-001.jpg"));
 		Graphics2D g = (Graphics2D) img.getGraphics();
-		g.setBackground(Color.white);
-		g.fillRect(0, 0, width, height);
 		g.setColor(Color.black);
-		g.drawLine(width / 3, 100, width / 3, height - 100);
-		g.drawLine(2 * width / 3, 100, 2 * width / 3, height - 100);
 		Font myFont = new Font("Times New Roman",Font.ROMAN_BASELINE, 35);
 		g.setFont(myFont);
-		g.drawString("Место для штампа организации  ",10,50);
-		g.drawString("Путевой лист автобуса ________ № ",width/3+10,50);
-		g.drawString("Типовая межотраслевая форма №6",width-550,50);
+		g.drawString("МУП МП ",500,420);
+
 		ImageIO.write(img, "gif", new File("image.jpg"));
 	}
 	@Override
 	public int print(Graphics g, PageFormat pf, int pageIndex) {
+
 		Graphics2D g2d = (Graphics2D) g;
 		g.translate((int) (pf.getImageableX()), (int) (pf.getImageableY()));
 		if (pageIndex == 0) {
@@ -57,8 +58,10 @@ public class Main implements Printable{
 			double scaleX = pageWidth / imageWidth;
 			double scaleY = pageHeight / imageHeight;
 			double scaleFactor = Math.min(scaleX, scaleY);
-			g2d.scale(scaleFactor, scaleFactor);
-			g.drawImage(img, 0, 0, null);
+			g2d.scale(scaleFactor*1.1, scaleFactor*1.1);
+			System.out.println(pf.getPaper().getImageableX() + " x " + pf.getPaper().getImageableY());
+			System.out.println(pf.getPaper().getImageableWidth() + " x " + pf.getPaper().getImageableHeight());
+			g2d.drawImage(img, 0, 0, null);
 			return Printable.PAGE_EXISTS;
 		}
 		return Printable.NO_SUCH_PAGE;
